05599c3bfd80279b488f217aa40f977a5eacf227
Erik Smith <eriksmith.career@hotmail.com>
2025-07-21 20:25:17 +0100

    update

--- chp2StoringData.py
+++ chp2StoringData.py
@@ -3,3 +3,8 @@ name = input('Please enter your name ')
 print('Hi', name)
 print('Welcome to Coding for beginners in easy steps')
 print('Remember to have fun', name , '!')
+name2 = input('Please enter your name ')
+
+print ('Hi' + name2 , end = '')
+print('Welcome to coding for beginners in easy steps')
+print('Remember to have fun' + name2 + '!')
