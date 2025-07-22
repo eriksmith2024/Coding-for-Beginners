401334e03c5e1e800a5defa0943c947a4a188ec7
Erik Smith <eriksmith.career@hotmail.com>
2025-07-22 10:04:05 +0100

    data conversion

---
+++ convertingData.py
@@ -0,0 +1,16 @@
+num1  = input('Please enter a whole number:')
+num2 = input('Now enter another whole number')
+
+print('Input is;', type(num1), type(num2))
+
+total = num1 + num2
+print( 'Total', total, type(total))
+
+total = int( num1 ) + int(num2)
+print('Total', total, type(total))
+
+total = float( num1 ) + float( num2 )
+print('Total', str(total), type(total))
+
+total = str(float(num1) + float(num2))
+print('Total', total, type(total))
