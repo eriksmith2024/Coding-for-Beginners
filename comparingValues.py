4f23f3d7e767260b531a40752a8ad416f1b39825
Erik Smith <eriksmith.career@hotmail.com>
2025-07-22 15:41:28 +0100

    Comnparing Values

---
+++ comparingValues.py
@@ -0,0 +1,16 @@
+nil = 0
+num = 0
+top = 1
+cap = 'A'
+low ='a'
+
+print('Equality: \t\t', nil, '==', num, nil == num)
+print('Equality: \t\t', cap, '==', low, cap == low)
+
+print('Inequality:\t\t', nil, '!=', top, nil != top)
+
+print('Greater: \t\t', nil, '>', top, nil > top)
+print('Lessser: \t\t', nil, '<', top, nil < top)
+
+print('More or Equal to: \t', nil, '>=', num, nil >= num)
+print('Less or Equal to: \t', top, '<=', num, top <= num)
