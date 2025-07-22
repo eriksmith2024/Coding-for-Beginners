2e42eccf1275a9cb10a6dc38b41e0633ea27b8e8
Erik Smith <eriksmith.career@hotmail.com>
2025-07-22 14:22:41 +0100

    arithmetic

---
+++ arithmetic.py
@@ -0,0 +1,16 @@
+a = 8
+b = 2
+
+print('Addition:\t', a, '+', b, '=', a + b )
+
+print('Subtraction:\t\t', a, "-", b, '=', a - b)
+
+print('Multiplication:\t\t\t', a, 'x', b, '=', a * b)
+
+print('Division:\t\t\t\t', a, '/', b, '=', a / b)
+
+print('Floor Division:\t\t\t\t\t', a, '//', b, '=', a // b)
+
+print('Remainder:\t\t\t\t\t\t', a, "%", b, '=', a % b)
+
+print('Exponent:\t\t\t\t\t\t', a, ' exp2 = ', a**b, sep ='')
