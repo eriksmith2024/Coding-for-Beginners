3afcbd272926b1df1ea6014b702ed2aba2319430
Erik Smith <eriksmith.career@hotmail.com>
2025-07-22 15:56:12 +0100

    Finding truth update

--- findingTruth.py
+++ findingTruth.py
@@ -6,3 +6,12 @@ print('a and a =', a and a)
 print('a and b =', a and b)
 print('b and b =', b and b)
 
+# the \n adds a line of space when printed out
+print('\nOR Logic') # If either this or that is true then do something
+print('a and a = ', a or a)
+print('a or b = ', a or b)
+print('b or b =', b or b)
+
+print('\nNOT Logic')
+print('a =', a, '\tnot a =', not a)
+print('b =', b, '\tnot b =', not b)
