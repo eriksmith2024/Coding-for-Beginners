105c65532abae09565929d9000cb1108d335c113
Erik Smith <eriksmith.career@hotmail.com>
2025-07-23 18:03:07 +0100

    dictionaries

--- namingElements.py
+++ namingElements.py
@@ -1,4 +1,18 @@
 info = { 'name' : 'Bob', 'ref':'Python', 'sys':'Win'}
-print('info:', type(info))
-print('Dictionary:',info)
+print('\ninfo:', type(info))
+print('\nDictionary:',info,)
+
+print('\nReference:',info['ref'])
+
+print('\nKeys:', info.keys())
+
+del info['name']
+print('\nNewDisctionary', info,'\n')# I added this to show where one of the key value pairs in the dictionary has been deleted
+info['user'] = 'Tom'
+print('\nDictionary:' , info)
+
+print('\nIs There is A name key?:', 'name' in info, '\n')
+
+
+
 
