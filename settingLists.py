420f56378bd1ca138850d19df8b40042c0b30e93
Erik Smith <eriksmith.career@hotmail.com>
2025-07-23 17:24:40 +0100

    sets

---
+++ settingLists.py
@@ -0,0 +1,6 @@
+party_goers = {'Andrew','Barbara','Carole','David'}
+print('\nparty_goers:', type(party_goers))
+
+print('\nDid David go to the party?','David' in party_goers)
+
+print('\nDid Kelly go to the party', 'Kelly' in party_goers)
