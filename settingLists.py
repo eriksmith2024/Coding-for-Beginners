bec810b0fe027bce5b2216f260f061e5522b160a
Erik Smith <eriksmith.career@hotmail.com>
2025-07-23 17:28:52 +0100

    sets update

--- settingLists.py
+++ settingLists.py
@@ -4,3 +4,11 @@ print('\nparty_goers:', type(party_goers))
 print('\nDid David go to the party?','David' in party_goers)
 
 print('\nDid Kelly go to the party', 'Kelly' in party_goers)
+
+students = {'Andrew', 'Kelly', 'Lynn', 'David'}
+
+commons = party_goers.intersection(students)
+party_students = list( commons)
+
+print('\nStudents at the party:', party_students)
+print('First student at the party:', party_students[0], '\n')
