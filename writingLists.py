7ced3ea1b3be202fa9b291401e289d5050d51006
Erik Smith <eriksmith.career@hotmail.com>
2025-07-23 16:15:19 +0100

    WritingLists

---
+++ writingLists.py
@@ -0,0 +1,14 @@
+quarter = ['January','February','March']
+
+print('Fisrt Month:', quarter[0])
+print('Second Month:', quarter[1])
+print('Third Month:',quarter[2])
+
+coords = [[1,2,3],[4,5,6],[7,8,9]]
+
+print('\nTop Left 0,0 : ',  coords[0][0])
+print('Middle Right 1,2 :', coords[1][2])
+print('Bottom Right 2,2 :', coords[2][2])
+
+
+print('\nSecond Month First Letter :', quarter[2][0])
