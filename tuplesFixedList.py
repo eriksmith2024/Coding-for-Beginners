303fa685fad457e49ed89961fe5ee3c937ed788a
Erik Smith <eriksmith.career@hotmail.com>
2025-07-23 17:04:51 +0100

    tuples

---
+++ tuplesFixedList.py
@@ -0,0 +1,7 @@
+days = ('Mon','Tue','Wed','Thu','Fri','Sat','Sun')
+print('\ndays', type(days))
+
+print('Days of the week;', days)
+print('No. of days in a week:', len(days))
+print('Start day of the week:',days[0],'\n')
+
