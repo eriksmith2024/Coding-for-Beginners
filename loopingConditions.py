b7838d48789b4b6161b16c8884c81f8258492b31
Erik Smith <eriksmith.career@hotmail.com>
2025-07-24 20:54:34 +0100

    looping conditions

--- loopingConditions.py
+++ loopingConditions.py
@@ -1,4 +1,14 @@
-a = b = 1
-while b < 100:
-    print(b)
-    a,b = b, a + b
+# a = b = 1
+# while b < 100:
+#     print(b)
+#     a,b = b, a + b
+
+i = 1
+while i < 4 :
+    print('Outer Loop Iteration:',i)
+    i += 1
+
+    j = 1
+    while j < 6 :
+        print('\tInner Loop Iteration', j )
+        j += 1
