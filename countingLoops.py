04d7da67a061f3acafeb3b642276d346d9b32795
Erik Smith <eriksmith.career@hotmail.com>
2025-07-24 20:33:04 +0100

    Counting Loops

--- countingLoops.py
+++ countingLoops.py
@@ -3,3 +3,13 @@ fruit = ('Apple','Banana','Cherry')
 info = {'name ': ' Mike', 'ref ': ' Python', 'sys' : 'Win'}
 
 print('Elements: \t', end = '')
+for i in chars:
+    print(i, end = ' ')
+
+print('\nEnumerated:\t', end = '')
+for item in enumerate( chars ):
+    print(item, end = ' ')
+
+print('\nZipped: \t' , end = ' ')
+for item in zip(chars , fruit) :
+    print( item ,  end = ' ')
