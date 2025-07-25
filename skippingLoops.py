d7de0d2d07cc83ee2c22a5b3f2801c0b5ba93dfa
Erik Smith <eriksmith.career@hotmail.com>
2025-07-25 10:57:55 +0100

    skipping loops exploring

--- skippingLoops.py
+++ skippingLoops.py
@@ -20,9 +20,9 @@
 for i in range(1, 4):  # Outer loop: i = 1 to 3
     for j in range(1, 4):  # Inner loop: j = 1 to 3
         if i == 1 and j == 2:
-            print('\nContinues inner loop at i=1 j=3')
+            print('\nSkips j=1 i = 2 then Continues inner loop at i=1 j=3')
             continue  # Skips the rest of this inner loop iteration
         if i == 3 and j == 2:
-            print('\nBreaks inner loop at i=2 j=1')
+            print('\nBreaks inner loop at i=3 j=2') # basically breaks the rest of that loop 
             break  # Exits the inner loop completely for this i
         print('Running i =', i, 'j =', j)
