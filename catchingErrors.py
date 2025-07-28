a33ace7b04f6075ee372fd0de892e0cdf777f197
Erik Smith <eriksmith.career@hotmail.com>
2025-07-28 14:24:05 +0100

    catching errors update

--- catchingErrors.py
+++ catchingErrors.py
@@ -1,8 +1,22 @@
-title = 'Coding for Beginners in easy steps'
+# title = 'Coding for Beginners in easy steps'
 
-try:
+# try:
 
-    print(titel)
+#     print(titel)
 
-except NameError as msg:
-    print( msg )
+# except NameError as msg:
+#     print( msg )
+
+
+day = 32
+
+try : 
+    if day > 31 :
+        raise ValueError( 'Invalid Day Number')
+        #More statements to execute get added here. 
+
+except ValueError as msg :
+    print( 'The program found An', msg)
+
+finally: 
+    print('But today is a beautiful day anyway')
