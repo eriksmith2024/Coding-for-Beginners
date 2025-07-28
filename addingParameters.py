a6959419bd1ce1ff394ff9856a0b13de40b4b4a7
Erik Smith <eriksmith.career@hotmail.com>
2025-07-28 15:04:05 +0100

    adding parameters update

---
+++ addingParameters.py
@@ -0,0 +1,6 @@
+def echo( user, lang, sys ):
+    print('\n','User:',user, 'Language:', lang, 'Platform', sys ,'\n')
+
+echo('Mike', 'Python', 'Windows')
+
+echo(lang ='Python', sys= 'Mac OS', user = 'Anne')
