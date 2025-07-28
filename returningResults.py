c06e23f74acb51b7fa8cdc2782e398691de4271a
Erik Smith <eriksmith.career@hotmail.com>
2025-07-28 16:01:41 +0100

    Returning Results updates

--- returningResults.py
+++ returningResults.py
@@ -1,10 +1,23 @@
-num = input('Enter an Integer:')
+# num = input('Enter an Integer:')
 
-def square( num ):
-    if not num.isdigit():
-        return 'Invalid Entry'
-    num = int(num)
+# # def square( num ):
+# #     if not num.isdigit():
+# #         return 'Invalid Entry'
+# #     num = int(num)
+# #     return num * num
+
+# # print( num, 'Squared is:', square( num ))
+
+
+
+def square(num):
     return num * num
 
-print( num, 'Squared is:', square( num ))
-
+while True:
+    num = input('Enter an Integer: ')
+    if num.isdigit():
+        num = int(num)
+        print(num, 'Squared is:', square(num))
+        break
+    else:
+        print('Invalid Entry. Please enter a valid integer.')
