730dbf852ad88469d2bad7d2c8e81e69e149347c
Erik Smith <eriksmith.career@hotmail.com>
2025-07-28 14:38:15 +0100

    Defining Blocks

---
+++ definingBlocks.py
@@ -0,0 +1,14 @@
+global_var = 1
+
+def my_vars():
+    print( 'Global variable:', global_var)
+
+    local_var = 2
+    print('Local variable:', local_var)
+
+    global inner_var
+    inner_var = 3
+
+my_vars()
+
+print('Second Global', inner_var)
