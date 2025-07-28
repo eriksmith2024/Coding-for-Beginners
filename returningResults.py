8337667f4eea1ce98c65d9153a0e7bc318b4ac3f
Erik Smith <eriksmith.career@hotmail.com>
2025-07-28 15:57:47 +0100

    Returning Results update

---
+++ returningResults.py
@@ -0,0 +1,10 @@
+num = input('Enter an Integer:')
+
+def square( num ):
+    if not num.isdigit():
+        return 'Invalid Entry'
+    num = int(num)
+    return num * num
+
+print( num, 'Squared is:', square( num ))
+
