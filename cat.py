87a732be2ac3c9eca071d592b108919b98d4b45f
Erik Smith <eriksmith.career@hotmail.com>
2025-07-28 16:07:56 +0100

    Storing Functions

---
+++ cat.py
@@ -0,0 +1,10 @@
+def purr( pet = 'A Cat'):
+    print(pet, 'Says MEOW')
+
+def lick(pet = 'A Cat'):
+    print(pet,'Drinks Milk')
+
+def nap( pet = 'A Cat'):
+    print(pet, 'Sleeps By the Fire')
+
+    
