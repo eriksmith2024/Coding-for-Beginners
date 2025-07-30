d6b4c58d2f66eef8dba1ede59809b2339adc38de
Erik Smith <eriksmith.career@hotmail.com>
2025-07-30 14:50:26 +0100

    bubble sorts comments

--- bubblingSorts.py
+++ bubblingSorts.py
@@ -1,17 +1,24 @@
-def bubble_sort( array) :
-    for index in range( len( array)):
+def bubble_sort(array):
+    # Outer loop runs once for each element in the array
+    for index in range(len(array)):
 
-        for element in range(( len( array )-1 ) - index):
-            if array[ element ] > array[ element+1 ] :
-                array[ element ], array[ element+1 ] = \
-                    array[ element+1 ], array[ element ]
-                
-                print( '\tResolving element[', element, \
-                      '] to ', array)
+        # Inner loop compares adjacent elements up to the unsorted portion
+        for element in range((len(array) - 1) - index):
+            # If the current element is greater than the next, swap them
+            if array[element] > array[element + 1]:
+                array[element], array[element + 1] = array[element + 1], array[element]
 
-array = [ 5, 3, 1, 2, 6, 4,]
-print( 'Bubbl;e Sort..\nArray :', array )
+                # Print the array status after each swap
+                print('\tResolving element[', element, '] to', array)
 
+# Define the unsorted array
+array = [5, 3, 1, 2, 6, 4]
 
-bubble_sort( array )
-print( 'Array: ', array )
+# Print the original array
+print('Bubble Sort..\nArray:', array)
+
+# Perform bubble sort
+bubble_sort(array)
+
+# Print the sorted array
+print('Array:', array)
