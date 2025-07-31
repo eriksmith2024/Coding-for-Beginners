063f88906b3ef8946239b5906efba0021970f1af
Erik Smith <eriksmith.career@hotmail.com>
2025-07-31 13:14:56 +0100

    maths module updates

--- maths.py
+++ maths.py
@@ -6,3 +6,9 @@ print( 'Rounded Down 9.5 will be', math.floor(9.5))
 num = 4
 print('\n',num, 'Squared:',math.pow(num, 2))
 print('\n',num,'Square Root:', math.sqrt(num))
+
+
+nums = random.sample( range( 1, 60 ),6 )
+print('\nYour Lucky Lotto Numbers Are:', nums, '\n')
+
+
