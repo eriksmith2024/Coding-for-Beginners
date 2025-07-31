0afee36d5399d4242cfde5b78bb2820cdac2f1be
Erik Smith <eriksmith.career@hotmail.com>
2025-07-31 13:43:01 +0100

    maths decimals fix

--- calculatingdecimals.py
+++ calculatingdecimals.py
@@ -1,4 +1,4 @@
-from decimal import
+from decimal import *
 
 item = 0.70
 rate = 1.05
@@ -15,3 +15,15 @@ print()
 print('Item:\t', '{:.20f}'.format( item ))
 print('Tax:\t', '{:.20f}'.format( tax ))
 print('Total:\t', '{:.20f}'.format( total ))
+
+item = Decimal(0.70)
+rate = Decimal(1.05)
+
+tax = item * rate
+total = item + tax
+
+print('\nCorrected data using maths Decimal')
+
+print('Item:\t', '{:.2f}'.format( item ))
+print('Tax:\t', '{:.2f}'.format( tax ))
+print('Total:\t', '{:.2f}'.format( total ))
