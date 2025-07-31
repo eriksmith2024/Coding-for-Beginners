bb96e01935d6781e3cd7530bd5ee3e21f047b51f
Erik Smith <eriksmith.career@hotmail.com>
2025-07-31 13:08:43 +0100

    maths module

--- inspectingPython.py
+++ inspectingPython.py
@@ -2,4 +2,12 @@ import sys, keyword
 
 print('\nPython Version:', sys.version )
 
-print('Python Interpreter Location', sys.executable)
+print('\nPython Interpreter Location', sys.executable)
+
+print('\nPython module path:')
+for folder in sys.path :
+    print( folder )
+
+print('Python Keywords:')
+for word in keyword.kwlist :
+    print( word )
---
+++ maths.py
@@ -0,0 +1,8 @@
+import math, random
+
+print( 'Rounded Up 9.5 will be', math.ceil(9.5))
+print( 'Rounded Down 9.5 will be', math.floor(9.5))
+
+num = 4
+print('\n',num, 'Squared:',math.pow(num, 2))
+print('\n',num,'Square Root:', math.sqrt(num))
