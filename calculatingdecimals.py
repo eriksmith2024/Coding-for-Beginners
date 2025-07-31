45997c82e3ff4cf6a908896b2a285c118db7cc48
Erik Smith <eriksmith.career@hotmail.com>
2025-07-31 13:36:11 +0100

    maths decimals

---
+++ calculatingdecimals.py
@@ -0,0 +1,15 @@
+item = 0.70
+rate = 1.05
+
+tax = item * rate
+total = item + tax
+
+print('Item:\t', '{:.2f}'.format( item ))
+print('Tax:\t', '{:.2f}'.format( tax ))
+print('Total:\t', '{:.2f}'.format( total ))
+
+print()
+
+print('Item:\t', '{:.20f}'.format( item ))
+print('Tax:\t', '{:.20f}'.format( tax ))
+print('Total:\t', '{:.20f}'.format( total ))
