506aff63f4ff0ec3c656ffbb5c467b0e99a16f2c
Erik Smith <eriksmith.career@hotmail.com>
2025-08-03 20:22:20 +0100

    manipulating strings

---
+++ manipulate.py
@@ -0,0 +1,18 @@
+print ()
+
+def display( s ):
+    '''Display an argument value.'''
+    print( s )
+
+display( display.__doc__ )
+
+display(r'C:\Program Files')
+
+display( '\nHello' + ' Python ')
+
+display( 'Python in easy steps\n' [7 :])
+
+display( 'P' in 'Python')
+display( 'p' in 'Python')
+
+display( '\nHello'[0 : 4])
