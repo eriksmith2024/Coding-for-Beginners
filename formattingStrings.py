b1f3319ac320f0b4078156b5ec5b747f4f234149
Erik Smith <eriksmith.career@hotmail.com>
2025-08-03 20:40:37 +0100

    formatting strings finished

--- formattingStrings.py
+++ formattingStrings.py
@@ -4,4 +4,19 @@
 
 snack = '{} and {}'.format( 'Burger', 'Fries')
 
-print( '\nReplaced:' snack )
+print( '\nReplaced:', snack )
+
+snack = '{1} and {0}'.format('Burger', 'Fries')
+
+print( 'Replaced:', snack )
+
+snack = '%s and %s' % ( 'Milk', 'Cookies')
+
+print( '\nSubstituted:', snack)
+
+snack = '{1} and {0}'.format('Milk', 'Cookies')
+
+print( 'Replaced:', snack )
+
+
+
