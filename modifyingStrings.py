5245500c2432740908a87a18ece16d436d59d0c3
Erik Smith <eriksmith.career@hotmail.com>
2025-08-03 20:59:54 +0100

    modifying strings updates

--- modifyingStrings.py
+++ modifyingStrings.py
@@ -7,3 +7,9 @@ print( '\nCentered:\t' , string.center(30, '*'))
 
 print( '\nUppercase:\t', string.upper())
 print( '\nJoined:\t\t', string.join('**'))
+
+print( '\nJustified:\t\t', string.rjust( 30, '*'))
+
+print( '\nReplaced:\t', string.replace( 's', '*'))
+
+print()
