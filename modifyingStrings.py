87f5547c6a781b852aa7f09685b1119d0f88e3db
Erik Smith <eriksmith.career@hotmail.com>
2025-08-03 20:56:17 +0100

    modifying strings update

---
+++ modifyingStrings.py
@@ -0,0 +1,9 @@
+string = 'coding for beginners on easy steps'
+
+print( '\nCapitilized:\t', string.capitalize())
+print( '\nTitled:\t\t' , string.title())
+print( '\nCentered:\t' , string.center(30, '*'))
+
+
+print( '\nUppercase:\t', string.upper())
+print( '\nJoined:\t\t', string.join('**'))
