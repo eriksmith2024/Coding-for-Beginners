06e72f2c968d359632ed9f15a972facf82eb06a7
Erik Smith <eriksmith.career@hotmail.com>
2025-08-04 21:32:00 +0100

    read write update

--- readwrite.py
+++ readwrite.py
@@ -6,3 +6,10 @@ poem = 'I never saw a man who looked\n'
 poem += 'With such a wistful eye\n'
 poem += 'Upon that little tent of blue\n'
 poem += 'Which prisoners call the sky\n'
+
+file = open( 'poem.txt', 'w')
+
+file.write( poem )
+file.close()
+
+file = open( 'poem.txt'), "r"
