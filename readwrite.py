d66a404a0dc94f1ffe10d5015ce403ccdae5fee4
Erik Smith <eriksmith.career@hotmail.com>
2025-08-04 21:29:54 +0100

    read write

---
+++ readwrite.py
@@ -0,0 +1,8 @@
+'''
+An excerpt from the Ballad of Reading Gaol.
+'''
+
+poem = 'I never saw a man who looked\n'
+poem += 'With such a wistful eye\n'
+poem += 'Upon that little tent of blue\n'
+poem += 'Which prisoners call the sky\n'
