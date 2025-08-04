2d423dc6bd6eb478042acf4b5f6629389933022e
Erik Smith <eriksmith.career@hotmail.com>
2025-08-04 21:24:02 +0100

    accessing files updates

--- access.py
+++ access.py
@@ -1,8 +1,17 @@
 file = open( 'example.txt', 'w')
 
-print( 'File Name:', file.name )
+print( '\nFile Name:', file.name )
 print( 'File Open Mode:', file.mode )
 
 print( 'Readable:', file.readable() )
 print( 'Writeable:', file.writable() )
 
+def get_status( f ):
+    if not f.closed :
+        return 'Open'
+    else:
+        return 'Closed'
+    
+print( 'File status:', get_status( file ))
+file.close()
+print('\nFile Status:', get_status( file ))
