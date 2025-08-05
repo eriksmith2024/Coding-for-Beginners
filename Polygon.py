fbf622f2f68bab4c2fe6a61f51eae6cf76e4ca5c
Erik Smith <eriksmith.career@hotmail.com>
2025-08-05 19:54:41 +0100

    class inheritance

---
+++ Polygon.py
@@ -0,0 +1,8 @@
+class Polygon :
+    width = 0
+    height = 0
+    def set_values( self , width, height):
+        self.width = width
+        self.height = height
+
+        
---
+++ Triangle.py
@@ -0,0 +1,7 @@
+from Polygon import *
+
+class Triangle( Polygon ) :
+    def area( self ) :
+        return ( self.width * self.height ) / 2
+    
+    
