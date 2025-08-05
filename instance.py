f2e5bfc0794a147c8a5650be1772f8a8fe7eaff7
Erik Smith <eriksmith.career@hotmail.com>
2025-08-05 19:31:52 +0100

    classes instances

--- instance.py
+++ instance.py
@@ -4,3 +4,10 @@ print('\nClass instances Of:\n', Bird.__doc__)
 
 polly = Bird('Squawk, squawk!')
 
+print('\nNumber of Birds:', Bird.count )
+print( 'Polly Says:', polly.talk())
+
+harry = Bird('Tweet, tweet')
+
+print('\nNumber of Birds:', Bird.count )
+print('Harry says:', harry.talk())
