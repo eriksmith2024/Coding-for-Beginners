8e2d54578faf275234084ac8f81937ad0fe40914
Erik Smith <eriksmith.career@hotmail.com>
2025-08-05 19:46:15 +0100

    classes instances update

---
+++ address.py
@@ -0,0 +1,20 @@
+from bird import *
+
+chick = Bird('Cheep, cheep')
+chick.age ='1 week'
+
+print( '\nChick says:', chick.talk())
+print( 'Chick Age:', chick.age)
+
+chick.age = '2 weeks'
+print('Chick now:', chick.age)
+
+setattr( chick, 'age', '3 weeks')
+
+print('\nChick Attributes...')
+for attrib in dir( chick ):
+    if attrib[0] != '_':
+        print( attrib, ':', getattr( chick, attrib))
+
+delattr( chick ,'age')
+print('\nChick age Attribute', hasattr( chick, 'age'))
