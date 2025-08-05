b52fb71c19189ff75f4682f61cebed99d46edfdb
Erik Smith <eriksmith.career@hotmail.com>
2025-08-05 20:45:22 +0100

    overriding methods

---
+++ Man.py
@@ -0,0 +1,8 @@
+from Person import *
+
+class Man( Person ):
+    '''A derived class to define Man properties'''
+    def speak( self, msg ):
+        print( self.name, '\n\tHello!', msg)
+
+        
---
+++ Person.py
@@ -0,0 +1,11 @@
+class Person:
+
+    '''A base class to define person properties'''
+
+    def __init__(self, name):
+        self.name = name
+        
+    def speak( self, msg = '(Calling the Base Class)' ):
+            print( self.name, msg )
+
+            
