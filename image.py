a264a9b588917e933e426a5694f73473d471d3ae
Erik Smith <eriksmith.career@hotmail.com>
2025-08-06 18:53:23 +0100

    images

--- image.py
+++ image.py
@@ -6,3 +6,8 @@ window.title( 'Image Example')
 img = PhotoImage( file = 'python.gif')
 
 label = Label( window, image = img, bg ='magenta')
+
+small_img = PhotoImage.subsample( img , x = 2, y = 2)
+
+btn = Button( window, image = small_img )
+
