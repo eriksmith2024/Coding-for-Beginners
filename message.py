c7c9dba839367caffe2aa62ef4dace1209388e64
Erik Smith <eriksmith.career@hotmail.com>
2025-08-06 15:25:09 +0100

    python buttons

---
+++ message.py
@@ -0,0 +1,22 @@
+from tkinter import *
+import tkinter.messagebox as box
+
+window = Tk()
+window.title('Message Box Example')
+# Bring window to front
+window.deiconify()
+window.lift()
+window.focus_force()
+
+def dialog() :
+    var = box.askyesno('Message Box', 'Proceed?')
+    if var == 1:
+        box.showinfo( 'Yes Box', 'Proceeding...')
+    else: 
+        box.showwarning('No Box', 'Cancelling...')
+
+btn = Button( window, text = 'Click', command=dialog)
+
+btn.pack( padx = 120, pady = 50 )
+
+window.mainloop()
