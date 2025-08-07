afecb91b8dc71091df666dba7cdf6905f16954d4
Erik Smith <eriksmith.career@hotmail.com>
2025-08-07 11:53:21 +0100

    random finish

--- sample.py
+++ sample.py
@@ -1,7 +1,18 @@
 from random import random, sample
 
 num = random()
-print ( '0.0 to 1.0 Random float ', num)
+print ( '\n0.0 to 1.0 Random float ', num)
 
 num = int(num * 10 )
-print( '0 to 9 Random integer ', num)
+print( '\n0 to 9 Random integer ', num)
+
+nums = [] ; i = 0
+while i < 6 :
+    nums.append(int(random() * 10 ) + 1)
+    i += 1
+print( '\n1 to 10 Random sample of integers:', nums)
+
+nums = sample( range( 1, 60 ), 6)
+print( '\n1 to 59 Random Integer Sample: ', nums)
+
+print()
